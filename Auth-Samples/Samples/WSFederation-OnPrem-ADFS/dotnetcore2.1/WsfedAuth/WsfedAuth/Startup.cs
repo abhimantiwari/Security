325daebde22ed91a0767ac117d0786b6bea9f037
Abhiman <abhtiwar@microsoft.com>
2020-05-01 01:56:54 +0530

    Initial Commit

---
+++ Auth-Samples/Samples/WSFederation-OnPrem-ADFS/dotnetcore2.1/WsfedAuth/WsfedAuth/Startup.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.WsFederation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WsfedAuth
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = WsFederationDefaults.AuthenticationScheme;
+            })
+                .AddWsFederation(options =>
+                {
+                    options.Wtrealm = "https:localhost:44307";  //Your website URL
+                    options.MetadataAddress = "https://YOUR_ADFS_DOMAIN.COM/FederationMetadata/2007-06/FederationMetadata.xml";
+                })
+                .AddCookie();
+
+            services.AddAuthorization(options =>
+            {
+                //options.FallbackPolicy = options.DefaultPolicy;
+            });
+            //services.AddControllersWithViews();
+             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            //app.UseRouting();
+            //app.UseAuthentication();
+            //app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllerRoute(
+            //        name: "default",
+            //        pattern: "{controller=Home}/{action=Index}/{id?}");
+            //});
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
