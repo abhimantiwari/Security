838441654387bba75b23ad830fab5fef13790b1a
Abhiman Tiwari <abhimankumar@gmail.com>
2020-07-02 21:11:09 +0530

    OAuth OIDC using IdentityServer and Blazor hosted auth with AAD

---
+++ Auth-Samples/Samples/OIDC and OAuth 2.0 with IdentityServer/dotnetcore3.1/DemoIdentityServer/src/IdentityServer/Config.cs
@@ -0,0 +1,72 @@
+﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+
+using IdentityServer4;
+using IdentityServer4.Models;
+using System.Collections.Generic;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> Ids =>
+            new IdentityResource[]
+            { 
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email()
+            };
+
+        public static IEnumerable<ApiResource> Apis =>
+            new ApiResource[] 
+            { 
+                //"api1.Read/Write"
+                new ApiResource ("api1","My API 1") 
+            };
+        
+        public static IEnumerable<Client> Clients =>
+           new List<Client>
+            {
+                // interactive ASP.NET Core MVC client
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "My MVC Client App",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+
+                    AllowedGrantTypes = GrantTypes.Hybrid,
+                    RequireConsent = true,
+                    //RequirePkce = true,
+                
+                    // where to redirect to after login
+                    RedirectUris = { "http://localhost:8097/signin-oidc" },
+                    
+                    // where to redirect to after logout
+                    PostLogoutRedirectUris = { "http://localhost:8097/signout-callback-oidc"},
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "api1"
+                    },
+                    AllowOfflineAccess = true
+                },
+
+                // machine to machine client
+                new Client
+                {
+                    ClientId = "client",
+                    ClientName = "My API",
+                    ClientSecrets = { new Secret("secret".Sha256()) },
+                    AllowedGrantTypes = GrantTypes.Code,
+
+                    // scopes that client has access to
+                    AllowedScopes = { "api1" },
+                    AllowOfflineAccess= true
+                }
+            };
+
+    }
+}
---
+++ Auth-Samples/Samples/OIDC and OAuth 2.0 with IdentityServer/dotnetcore3.1/DemoIdentityServer/src/MvcClient/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcClient.Models;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace MvcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            //HttpContext.AuthenticateAsync.ChallengeAsync();
+            //return new ChallengeResult();
+
+            return Challenge(new AuthenticationProperties
+            {
+                RedirectUri = "/Home/Index"
+            }, "oidc");
+        }
+
+        public async Task<IActionResult> CallApi()
+        {
+            var accessToken = await HttpContext.GetTokenAsync("access_token");
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+            var content = await client.GetStringAsync("http://localhost:54321/identity");
+
+            ViewBag.Json = JArray.Parse(content).ToString();
+            return View("json");
+        }
+
+        /// <summary>
+        /// Calling MVC and Identity server middleware to delete cookies and signout
+        /// IdSvr:end_session_endpoint
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Logout()
+        {
+            return SignOut(new AuthenticationProperties
+            {
+                RedirectUri = "/Home/Index"
+            }, "Cookies", "oidc");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
